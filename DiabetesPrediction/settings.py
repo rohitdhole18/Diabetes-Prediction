e35bc4da1a51bbdb5d0b63e810bdf9340a8d12d3
rohit dhole <rohitdhole570@gmail.com>
2022-04-24 20:22:46 +0530

    third

--- DiabetesPrediction/settings.py
+++ DiabetesPrediction/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os.path
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,4 +125,8 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
